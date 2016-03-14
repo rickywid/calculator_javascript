82698364c2845ac8bb83a1f6eb8bb9d7ebd6f45e
Ricky JW <rickywid@hotmail.com>
2016-03-13 21:00:48 -0400

    move click event from html to js

--- index.js
+++ index.js
@@ -1,26 +1,95 @@
 /* Simple Javascript Calculator */
 
 
+var one = document.getElementById('1');
+var two = document.getElementById('2');
+var three = document.getElementById('3');
+var four = document.getElementById('4');
+var five = document.getElementById('5');
+var six = document.getElementById('6');
+var seven = document.getElementById('7');
+var eight = document.getElementById('8');
+var nine = document.getElementById('9');
+var zero = document.getElementById('0');
+
+var plus = document.getElementById('plus');
+var minus = document.getElementById('minus');
+var divide = document.getElementById('divide');
+var multiply = document.getElementById('multiply');
+
+var equalBtn = document.getElementById('equals');
+var clearBtn = document.getElementById('clear');
+var result = document.getElementById('result');
+var decimal = document.getElementById('decimal')
+
+var total;
 
 function calc(num){
+		
+		result.value += num;
+		
+		
+		equalBtn.addEventListener('click', function(){
+			total = eval(result.value);
+			result.value = total;
+		})
 
-	var result = document.getElementById('result');
-	result.value += num;
+		clearBtn.addEventListener('click', function(){
+			result.value = "";
+		})
+	}
 
+plus.addEventListener('click', function(){
+	calc('+');
+})
 
-	var total = eval(document.frm.result.value);
+minus.addEventListener('click', function(){
+	calc('-');
+})
 
-	
-	var equalBtn = document.getElementById('equals');
-	var clearBtn = document.getElementById('clear');
+divide.addEventListener('click', function(){
+	calc('/');
+})
 
-	equalBtn.addEventListener('click', function(){
-		result.value = total;
+multiply.addEventListener('click', function(){
+	calc('*');
+})
 
+one.addEventListener('click', function(){
+	calc(1);
+})
 
-	})
+two.addEventListener('click', function(){
+	calc(2);
+})
+three.addEventListener('click', function(){
+	calc(3);
+})
 
-	clearBtn.addEventListener('click', function(){
-		result.value = "";
-	})
-}
+four.addEventListener('click', function(){
+	calc(4);
+})
+five.addEventListener('click', function(){
+	calc(5);
+})
+
+six.addEventListener('click', function(){
+	calc(6);
+})
+seven.addEventListener('click', function(){
+	calc(7);
+})
+
+eight.addEventListener('click', function(){
+	calc(8);
+})
+nine.addEventListener('click', function(){
+	calc(9);
+})
+zero.addEventListener('click', function(){
+	calc(0);
+})
+
+decimal.addEventListener('click', function(){
+	calc('.');
+})
